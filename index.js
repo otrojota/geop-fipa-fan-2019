d870353e89ce986f5296c0f11d4016c4c1441963
Jorge Jimenez <jota@MacJota.local>
2020-05-26 10:34:29 -0400

    update

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 global.confPath = __dirname + "/config.json";
 global.resDir = __dirname + "/resources";
 const config = require("./lib/Config").getConfig();
-const ProveedorCapasBCN = require("./lib/ProveedorCapasFIPAFAN2019");
+const ProveedorCapasFIPA = require("./lib/ProveedorCapasFIPAFAN2019");
 
-const proveedorCapas = new ProveedorCapasBCN({
+const proveedorCapas = new ProveedorCapasFIPA({
     puertoHTTP:config.webServer.http.port,
     directorioWeb:__dirname + "/www",
     directorioPublicacion:null
 });
-proveedorCapas.start();
+
+if (process.argv.length == 4) {
+    if (process.argv[2] == "-cmd") {
+        let a = process.argv[3];
+        proveedorCapas[a]();
+    }
+} else {
+    proveedorCapas.start();
+}
